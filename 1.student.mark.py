780f3a180946c82d0af55e9fbae5d1d7fb4d61f2
ctn3m0 <hieuquang15801@gmail.com>
2021-03-27 19:52:44 +0700

    Change variable name

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,4 +1,4 @@
-classt = {
+class_student = {
     "number" : 0,
     "students" : []
 }
@@ -8,67 +8,65 @@ course = {
     "courses" : []
 }
 
-def sinfo():
-    si = int(input("Enter student id: "))
-    n = input("Enter student name: ")
-    dob = input("Enter student DoB: ")
-    st = {
-        "id" : si,
-        "name" : n,
-        "DoB" : dob,
+def student_info():
+    student_id = int(input("Enter student's id: "))
+    name = input("Enter student name: ")
+    date_of_birth = input("Enter student's DoB: ")
+    student = {
+        "id" : student_id,
+        "name" : name,
+        "DoB" : date_of_birth,
         "courses": [],
     }
-    return st
+    return student
 
-def slist():
+def student_list():
     print("\nWe have the list of students: ")
-    for i in classt["students"]:
-        print(f'ID: {i["id"]} \nName: {i["name"]} \nDoB: {i["DoB"]} \nCourse: {i["courses"]}\n')
+    for student in class_student["students"]:
+        print(f'ID: {student["id"]} \nName: {student["name"]} \nDoB: {student["DoB"]} \nCourse: {student["courses"]}\n')
 
-def cnumber_students():
-    s = int(input("How many student?\n- ")) 
-    classt["number"] = s
-    for i in range(s):
-        classt["students"].append(sinfo())
+def class_number_students():
+    student_number = int(input("How many student?\n- ")) 
+    class_student["number"] = student_number
+    for i in range(student_number):
+        class_student["students"].append(student_info())
 
 def list_course():
-    n = int(input("Enter number of course: "))
-    course["number"] = n
-    for i in range(n):
-        x = input("Enter course name: ")
-        y = int(input("Enter course id: "))
-        course["courses"].append([x,y])
+    course_number = int(input("Enter number of course: "))
+    course["number"] = course_number
+    for i in range(course_number):
+        course_name = input("Enter course name: ")
+        course_id = int(input("Enter course id: "))
+        course["courses"].append([course_name,course_id])
 
-def clist():
-    for i in course["courses"]:
-        print(f'ID: {i[1]} --- Name: {i[0]}')
+def course_list():
+    for cour in course["courses"]:
+        print(f'ID: {cour[1]} --- Name: {cour[0]}')
         
-def cmark():
+def course_mark():
     print("\nWe have courses: ")
-    clist()
-    ind = int(input("\nPlease choose the course id to enter mark: "))
-    std = input("Please enter student name: ")
-    m = int(input("Please enter student's mark: "))
-    n = ""
-    for i in course["courses"]:
-        if i[1] == ind:
-            n = i[0]
-            i.append([std, m])
-    for i in classt["students"]:
-        if i["name"] == std:
-            i["courses"].append([n, m])
+    course_list()
+    course_id = int(input("\nPlease enter course's id to enter mark: "))
+    student_name = input("Please enter student's name: ")
+    mark = int(input("Please enter student's mark: "))
+    course_name = ""
+    for cour in course["courses"]:
+        if cour[1] == course_id:
+            course_name = cour[0]
+            cour.append([student_name, mark])
+    for student in class_student["students"]:
+        if student["name"] == student_name:
+            student["courses"].append([course_name, mark])
 
-def show_m():
-    ind = int(input("Please enter course id to see the marks: "))
-    for i in course["courses"]:
-        if i[1] == ind:
-            for x in range(2,len(i)):
-                print(i[x])
-cnumber_students()
-slist()
+def show_mark():
+    course_id = int(input("Please enter course id to see the marks: "))
+    for cour in course["courses"]:
+        if cour[1] == course_id:
+            for i in range(2,len(cour)):
+                print(cour[i])
+                
+class_number_students()
+student_list()
 list_course()
-
-cmark()
-slist()
-
-show_m()
+course_mark()
+show_mark()
